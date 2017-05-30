a5befc6ab3655fa6662a5cd36be9f3e52622c1c5
Sreenath Krishnan <sreenath.krishnan@10xgenomics.com>
2017-05-30 14:51:21 -0700

    Implemented SWA with affine gap penalty and a simple unit test

--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,12 @@
 
-extern crate nalgebra as na;
 
-const WORST_CASE_MAX_SCORE : i32 = 0;
+extern crate smith_waterman;
 
-#[derive(Copy, Clone)]
-enum Moves {
-    MATCH,
-    INSERT,
-    DELETE,
-    EMPTY
-}
-
-struct OptionScores {
-    match_opt  : i32,
-    insert_opt : i32,
-    delete_opt : i32
-}
-
-impl OptionScores {
-    fn best_option(&self) -> (i32, Moves) {
-        let mut best_move = Moves::EMPTY;
-        let mut best_score = WORST_CASE_MAX_SCORE;
-
-        if self.match_opt > best_score {
-            best_score = self.match_opt;
-            best_move = Moves::MATCH;
-        }
-        if self.insert_opt > best_score {
-            best_score = self.insert_opt;
-            best_move = Moves::INSERT;
-        }
-        if self.delete_opt > best_score {
-            best_score = self.delete_opt;
-            best_move = Moves::DELETE;
-        }
-        (best_score, best_move)
-    }
-}
-
-struct Alignment {
-    score_matrix : Vec<Vec<i32> >,
-    moves_matrix : Vec<Vec<Moves> >,
-    start_position : (i32, i32),
-    end_position   : (i32, i32),
-    score : i32,
-    moves : Vec<Moves>
-}
-
-impl Alignment {
-    fn new(m : usize, n : usize) -> Alignment { // m and n are the text lengths + 1
-        Alignment {
-            score_matrix : vec![vec![0; n]; m],
-            moves_matrix : vec![vec![Moves::EMPTY; n]; m],
-            start_position : (-1,-1),
-            end_position   : (-1,-1),
-            score : WORST_CASE_MAX_SCORE,
-            moves : Vec::new()
-        }
-    }
-    fn backtrace(&mut self) {
-        unimplemented!();
-    }
-}
-
-fn match_score( x: &u8, y: &u8 ) -> i32 {
-    if x==y { 2 } else { -1 }
-}
-fn indel_score( x: &u8) -> i32 {
-    match x {
-        _ => -1
-    }
-}
-
-fn naive_swa( s: &[u8], t: &[u8]) {
-
-    let ns = s.len() + 1; // 1 for blank prefix
-    let nt = t.len() + 1;
-
-    let mut align = Alignment::new(ns, nt);
-
-    let mut S = &mut align.score_matrix;
-    let mut M = &mut align.moves_matrix;
-
-    for i in 1..ns {
-        let x = s[i-1];
-        for j in 1..nt {
-            let y = t[j-1];
-            let opts = OptionScores {
-                match_opt  : S[i-1][j-1] + match_score(&x, &y),
-                insert_opt : S[i-1][ j ] + indel_score(&x),
-                delete_opt : S[ i ][j-1] + indel_score(&y)
-            };
-            // Currently not tracking parent
-            let (score, mov) = opts.best_option(); 
-            S[i][j] = score;
-            M[i][j] = mov;
-            if score > align.score {
-                align.score = score;
-                align.end_position = ( (i-1) as i32, (j-1) as i32);
-            }
-        }
-    }
-    println!(" Best score = {} at end position ({},{})", align.score, align.end_position.0, align.end_position.1);
-}
+use smith_waterman::semiglobal::SemiglobalAlign;
 
 fn main() {
-    let s = b"abccefg"; // Type [u8]
-    let t = b"abcxdexg";
-    naive_swa(s, t);
+    let s = b"ACCGTGGATGGG";
+    let t = b"GAAAACCGTTGAT";
+    let mut align = SemiglobalAlign::compute(s, t);
+    align.pretty_print(s,t);
 }
---
+++ src/semiglobal.rs
@@ -0,0 +1,249 @@
+
+use std::i32;
+use std::cmp::max;
+
+// Bunch of constants
+const NEGATIVE_INF : i32 = i32::MIN / 2; // Dividing by 2 to stay away from the overflow region
+
+// ************* Scoring scheme ************** //
+const GAP_INITIATION_SCORE : i32 = -5;
+const GAP_UNIT_SCORE       : i32 = -1;
+const MATCH_SCORE          : i32 = 1;
+const MISMATCH_SCORE       : i32 = -1;
+
+// ************* Allowed Moves ************** //
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Copy, Clone)]
+enum Moves {
+    MATCH,
+    SUBS,
+    INSERT,
+    DELETE,
+    NONE
+}
+
+// We are tying to align the read string "t" with the reference "s".
+// The read string "t" needs to be consumed completely and hence this
+// is a semi-global alignment. 
+//
+// An affine gap score model is used so that the gap score for a length 'k' is:
+// GapScore(k) = GAP_INITIATION_SCORE + GAP_UNIT_SCORE * k 
+//
+// score_matrix (i,j) is the best semiglobal alignment for prefixes s[0..i], t[0..j] 
+//
+// match_matrix(i,j) is the best score such that s[i] and t[j] ends in a match
+//              .... A   G  s_i
+//              .... C   G  t_j
+//
+// insert_matrix(i,j) is the best score such that s[i] is aligned with a gap
+//              .... A   G  s_i
+//              .... G  t_j  - 
+// This is interpreted as an insertion into "t" w.r.t reference "s" and hence
+// called the insert_matrix
+//
+// delete_matrix(i,j) is the best score such that t[j] is aligned with a gap
+//              .... A  s_i  -
+//              .... G   G  t_j 
+// This is interpreted as a deletion from "t" w.r.t reference "s" and hence
+// called the delete_matrix
+
+pub struct SemiglobalAlign {
+    // DP Matrices, could be made more memory efficient by just tracking previous row only
+    score_matrix  : Vec<Vec<i32> >,
+    match_matrix  : Vec<Vec<i32> >,
+    insert_matrix : Vec<Vec<i32> >,
+    delete_matrix : Vec<Vec<i32> >,
+
+    // This matrix need to consructed completely to allow for traceback unless we plan to
+    // use a fancier algorithm like Hirshberg's
+    moves_matrix  : Vec<Vec<Moves> >,
+
+    // Alignment Outputs
+    score   : i32,
+    s_range : [i32; 2],
+    t_range : [i32; 2],
+    moves   : Vec<Moves>
+}
+
+impl SemiglobalAlign {
+    fn new(m : usize, n : usize) -> SemiglobalAlign { // m and n are the text lengths + 1
+        SemiglobalAlign {
+            score_matrix  : vec![vec![0; n]; m],
+            match_matrix  : vec![vec![0; n]; m],
+            insert_matrix : vec![vec![0; n]; m],
+            delete_matrix : vec![vec![0; n]; m],
+            moves_matrix  : vec![vec![Moves::NONE; n]; m],
+
+            score   : NEGATIVE_INF,
+            s_range : [-1, -1], // 2nd index is exclusive
+            t_range : [-1, -1], // 2nd index is exclusive
+            moves   : Vec::new()
+        }
+    }
+    #[allow(non_snake_case)]
+    pub fn compute( s: &[u8], t: &[u8] ) -> SemiglobalAlign {
+
+        let m = s.len() + 1; // 1 for blank prefix
+        let n = t.len() + 1;
+
+        let mut align = SemiglobalAlign::new(m, n);
+
+        {
+            let mut S = &mut align.score_matrix;
+            let mut M = &mut align.match_matrix;
+            let mut I = &mut align.insert_matrix;
+            let mut D = &mut align.delete_matrix;
+            let mut P = &mut align.moves_matrix; // P for Parent, M is taken
+
+            // Inititalize the matrices
+            M[0][0] = 0;
+            I[0][0] = NEGATIVE_INF;
+            D[0][0] = NEGATIVE_INF;
+            S[0][0] = max ( max ( M[0][0] , I[0][0] ), D[0][0] );
+            P[0][0] = Moves::NONE;
+
+            for i in 1..m {
+                M[i][0] = NEGATIVE_INF;
+                I[i][0] = GAP_INITIATION_SCORE + GAP_UNIT_SCORE * (i as i32);
+                D[i][0] = NEGATIVE_INF;
+                S[i][0] = I[i][0];
+                P[i][0] = Moves::INSERT;
+            }
+            for j in 1..n {
+                M[0][j] = NEGATIVE_INF;
+                I[0][j] = NEGATIVE_INF;
+                D[0][j] = GAP_INITIATION_SCORE + GAP_UNIT_SCORE * (j as i32);
+                S[0][j] = D[0][j];
+                P[0][j] = Moves::DELETE;
+            }
+
+            // Core alignment computation
+            for i in 1..m {
+                let x = s[i-1];
+                for j in 1..n {
+                    let y = t[j-1];
+                    let insert_opt = ( max ( I[i-1][j],                       // Already in the insert mode - no initiation
+                                             S[i-1][j] + GAP_INITIATION_SCORE // Or in some other mode
+                                             ) + GAP_UNIT_SCORE,              // Unit score need to be added irrespective
+                                       Moves::INSERT);
+
+                    let delete_opt = ( max ( D[i][j-1],                       // Already in the delete mode - no initiation
+                                             S[i][j-1] + GAP_INITIATION_SCORE // Or in some other mode
+                                             ) + GAP_UNIT_SCORE,              // Unit score need to be added irrespective
+                                       Moves::DELETE);
+
+                    let match_opt = if x==y { 
+                                        (S[i-1][j-1] + MATCH_SCORE, Moves::MATCH) 
+                                    } else { 
+                                        (S[i-1][j-1] + MISMATCH_SCORE, Moves::SUBS)
+                                    };
+                    let best_opt = max( max( insert_opt, delete_opt ), match_opt ); // There is implicit tie-breaking in this logic
+
+                    I[i][j] = insert_opt.0;
+                    D[i][j] = delete_opt.0;
+                    M[i][j] = match_opt.0;
+                    S[i][j] = best_opt.0;
+                    P[i][j] = best_opt.1;
+
+                }
+            }
+        }
+
+        // It's traceback time
+        // In the semiglobal alignment setting, we should scan along j=n and pick the best score
+        // Then traceback the moves until j > 0
+        align.t_range = [0, (n-1) as i32];
+
+        for i in 0..m {
+            if align.score_matrix[i][n-1] > align.score {
+                align.score = align.score_matrix[i][n-1];
+                align.s_range[1] = i as i32;
+            }
+        }
+
+        let mut i : usize = align.s_range[1] as usize;
+        let mut j : usize = (n-1) as usize ;
+
+        while j > 0 {
+            align.moves.push(align.moves_matrix[i][j]);
+            match align.moves_matrix[i][j] {
+                Moves::MATCH => { i-=1; j-=1; },
+                Moves::SUBS  => { i-=1; j-=1; },
+                Moves::INSERT => { i-=1; },
+                Moves::DELETE => { j-=1; },
+                Moves::NONE => panic!("Moves should not be NONE. This is a terrible mistake! :/")
+            }
+        }
+        align.s_range[0] = i as i32;
+        align.moves.reverse();
+
+        align
+    }
+
+    pub fn pretty_print(&self, s: &[u8], t: &[u8]) {
+
+        println!(" Best score = {} ", self.score);
+        let mut line1 = Vec::new();
+        let mut line2 = Vec::new();
+        let mut line3 = Vec::new();
+
+        let mut i = self.s_range[0] as usize;
+        let mut j = self.t_range[0] as usize;
+        for m in &self.moves {
+            match *m {
+                Moves::MATCH => { 
+                    line1.push(s[i] as char);
+                    line2.push('|');
+                    line3.push(t[j] as char);
+                    i+=1; j+=1; 
+                },
+                Moves::SUBS  => { 
+                    line1.push(s[i] as char);
+                    line2.push('\\');
+                    line3.push(t[j] as char);
+                    i+=1; j+=1; 
+                },
+                Moves::INSERT => { 
+                    line1.push(s[i] as char);
+                    line2.push('+');
+                    line3.push('-');
+                    i+=1; 
+                },
+                Moves::DELETE => { 
+                    line1.push('-');
+                    line2.push('x');
+                    line3.push(t[j] as char);
+                    j+=1;
+                },
+                Moves::NONE => panic!("Moves should not be NONE. This is a terrible mistake! :/")
+            }
+        }
+
+        for l in line1 {
+            print!("{}",l);
+        }
+        println!("");
+
+        for l in line2 {
+            print!("{}",l);
+        }
+        println!("");
+
+        for l in line3 {
+            print!("{}",l);
+        }
+        println!("");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::Moves::*;
+    #[test]
+    fn simple_test_semiglobal() {
+        let s = b"ACCGTGGATGGG";
+        let t = b"GAAAACCGTTGAT";
+        let mut align = SemiglobalAlign::compute(s, t);
+        assert_eq!(align.moves, [DELETE, DELETE, DELETE, DELETE, MATCH, MATCH, MATCH, MATCH, MATCH, SUBS, MATCH, MATCH, MATCH] );
+    }
+}
